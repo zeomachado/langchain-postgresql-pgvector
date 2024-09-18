f2dc10acf8ac6f2a56bc728bcbc196652f9aa2fe
zeomachado <zeomachado@gmail.com>
2024-09-18 16:37:08 -0300

    Add First Embedding

--- pgvector-demo.py
+++ pgvector-demo.py
@@ -4,7 +4,9 @@
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 #from langchain.embeddings import OpenAIEmbeddings  #deprecated
-from langchain_community.embeddings import OpenAIEmbeddings
+#from langchain_community.embeddings import OpenAIEmbeddings #deprecated
+from langchain_openai import OpenAIEmbeddings
+
 from dotenv import load_dotenv
 #import os
 
@@ -14,11 +16,19 @@ load_dotenv()
 
 
 def main():
-    #load_dotenv()
-    documents = load_doc()
-    text_splitter(documents)
+    ##load_dotenv()
+    documents = load_docs()
+    splitted_documents = text_splitter(documents)
+    embeddings = OpenAIEmbeddings()
+    #vector = embeddings.embed_query('testing the embedding model')
+    #print(vector)
+    #print(len(vector))
 
-def load_doc():
+    #only the first 5 to save tokens for this example
+    doc_vectors = embeddings.embed_documents([t.page_content for t in splitted_documents[:2]])
+    print(doc_vectors)
+
+def load_docs():
 
     load_dotenv()
     loader = TextLoader('transcrever_YouTube_video_IA.txt', encoding='utf-8')
@@ -29,10 +39,11 @@ def load_doc():
     return documents
 
 def text_splitter(documents):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100 )
-    texts = text_splitter.split_documents(documents)
+    textSplitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100 )
+    splitted_texts = textSplitter.split_documents(documents)
     #print(texts[0])
-    print(len(texts))
+    #print(len(texts))
+    return splitted_texts
 
 if __name__=="__main__":
      main()
